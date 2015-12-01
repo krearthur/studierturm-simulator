f186c6b59fab45a5f4a05afe82567e48bd1475cf
arthur <artgrohe@googlemail.com>
2015-12-01 01:56:43 +0100

    Wand texturen

---
+++ Assets/Scripts/ToggleActivation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleActivation : MonoBehaviour {
+
+    public GameObject[] targetObjects;
+    public bool onTriggerSetActiveTo = false;
+    public string playerTag = "Player";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.CompareTag(playerTag)) {
+            foreach (GameObject section in targetObjects) { 
+                section.SetActive(onTriggerSetActiveTo);
+            }   
+        }
+    }
+}
