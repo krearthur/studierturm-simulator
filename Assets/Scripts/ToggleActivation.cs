2b076ebd99bc1cb3dbc633ff6b82ebce8521739b
arthur <artgrohe@googlemail.com>
2015-12-14 23:33:38 +0100

    toggle activation of colliders and gameobjects enhanced

--- Assets/Scripts/ToggleActivation.cs
+++ Assets/Scripts/ToggleActivation.cs
@@ -5,26 +5,71 @@ public class ToggleActivation : MonoBehaviour {
 
     public GameObject[] activateObjects;
     public GameObject[] deactivateObjects;
+
+    public GameObject[] activateCollidersInGameObjects;
+    public GameObject[] deactivateCollidersInGameObjects;
+
+    public ToggleActivation invertFromToggle;
+    public ToggleActivation copyFromToggle;
+
     public string playerTag = "Player";
+    public bool triggerOnStart = false;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	    if(invertFromToggle != null) {
+            activateObjects = invertFromToggle.deactivateObjects;
+            deactivateObjects = invertFromToggle.activateObjects;
+            activateCollidersInGameObjects = invertFromToggle.deactivateCollidersInGameObjects;
+            deactivateCollidersInGameObjects = invertFromToggle.activateCollidersInGameObjects;
+        }
+        if(copyFromToggle != null) {
+            activateObjects = invertFromToggle.activateObjects;
+            deactivateObjects = invertFromToggle.deactivateObjects;
+            activateCollidersInGameObjects = invertFromToggle.activateCollidersInGameObjects;
+            deactivateCollidersInGameObjects = invertFromToggle.deactivateCollidersInGameObjects;
+        }
+        if (triggerOnStart) {
+            Toggle();
+        }
 	}
 
+    private void Toggle() {
+        foreach (GameObject section in activateObjects) {
+            section.SetActive(true);
+        }
+        foreach (GameObject section in deactivateObjects) {
+            section.SetActive(false);
+        }
+        
+
+        foreach (GameObject section in activateCollidersInGameObjects) {
+            ActivateColliders(section.GetComponents<Collider>());
+        }
+        foreach (GameObject section in deactivateCollidersInGameObjects) {
+            DeactivateColliders(section.GetComponents<Collider>());
+        }
+        
+        
+    }
+	
     void OnTriggerEnter(Collider col) {
         if (col.CompareTag(playerTag)) {
-            foreach (GameObject section in activateObjects) { 
-                section.SetActive(true);
-            }
-            foreach (GameObject section in deactivateObjects) {
-                section.SetActive(false);
-            }
+            Toggle();
+        }
+    }
+
+    private void DeactivateColliders(Collider[] colliders) {
+        foreach (Collider collider in colliders) {
+            collider.enabled = false;
+            DeactivateColliders(collider.GetComponentsInChildren<Collider>(false));
+        }
+    }
+
+    private void ActivateColliders(Collider[] colliders) {
+        foreach (Collider collider in colliders) {
+            collider.enabled = false;
+            ActivateColliders(collider.GetComponentsInChildren<Collider>(true));
         }
     }
 }
